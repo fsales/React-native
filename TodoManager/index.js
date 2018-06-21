5a8846c3c1d78d6055d619689be1bb9bb8a56d57
Fábio Sales <fabio.oliveira.sales@gmail.com>
2018-06-20 23:33:30 -0300

    no message

--- TodoManager/index.js
+++ TodoManager/index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
 //import App from './App';
-import App from './src'
+import App from './src/index'
+import Register from './src/Register'
 
-AppRegistry.registerComponent('TodoManager', () => App);
+AppRegistry.registerComponent('TodoManager', () => Register);
